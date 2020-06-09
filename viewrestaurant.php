d8eb76f82cc3542e24e848820ec79313caa87ec1
Liya Dayana <55177516+katayomi@users.noreply.github.com>
2020-06-09 12:33:36 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -1,14 +1,18 @@
 <?php
   $serverName = "testdbtp043725.windows.net";
+
   $connectionOptions = array(
   "Database" => "testdbtp043725",
   "Uid" => "apuadmin",
   "PWD" => "Cay_008020");
+
   //Establishes the connection
   $conn = sqlsrv_connect($serverName, $connectionOptions);
   if (!$conn)
   {
   die("Error connection: ".sqlsrv_errors());
   }
+  else{
   echo "DB Server Connected";
+  }
 ?>
