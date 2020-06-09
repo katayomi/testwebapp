7017374051791b912343ed75eddb0eda1bc79474
Liya Dayana <55177516+katayomi@users.noreply.github.com>
2020-06-09 12:29:57 +0800

    Create viewrestaurant.php

---
+++ viewrestaurant.php
@@ -0,0 +1,15 @@
+<?php
+  $serverName = "testdbtp043725.windows.net";
+  $connectionOptions = array(
+  "Database" => "testdbtp043725",
+  "Uid" => "apuadmin",
+  "PWD" => "Cay_008020");
+  //Establishes the connection
+  $conn = sqlsrv_connect($serverName, $connectionOptions);
+  if (!$conn)
+  {
+  die("Error connection: ".sqlsrv_errors());
+  }
+  echo "DB Server Connected";
+ }
+?>
